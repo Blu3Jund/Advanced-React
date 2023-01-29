5d2d25be99cbdb0aa4db8b00b89a24fa7ea85b90
Blu3Jund <benjaminshawki@gmail.com>
2023-01-29 19:15:52 +0100

    feat: Form and use state

--- doblue/frontend/pages/sell.js
+++ doblue/frontend/pages/sell.js
@@ -1,3 +1,9 @@
+import CreateProduct from '../components/CreateProduct';
+
 export default function SellPage() {
-  return <p>Hello from the Sell page</p>;
+  return (
+    <div>
+      <CreateProduct />
+    </div>
+  );
 }
